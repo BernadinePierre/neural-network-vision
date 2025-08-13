fcfb37b1678d951b387e0ac1071b3097553652cd
bernadinepierre <bernadinepierre@gmail.com>
2025-08-13 13:25:50 +0100

    generated image dataset function to return  a number of samples for eeach image type

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 # neural-network-vision
 Training a neural network to recognise images
-=======
 Sprint instructions are found here: https://l2c.northcoders.com/courses/de-ng/py-neural-nets
->>>>>>> 090355d (Inital commit with data generation functions)
--- data.py
+++ data.py
@@ -13,5 +13,44 @@ def generate_horizontal_line():
     horizontal_line_img = blank_img
     return horizontal_line_img
 
-print("Here's a blank image:\n", generate_blank())
-print("Here's a horizontal line image:\n", generate_horizontal_line())
+# Example Labels:
+# 0 = blank
+# 1 = horizontal_line
+# ...etc... (when you add more types, you can add more labels)
+
+#Create a function which returns a number of samples of every type, each flattened, along with labels for which type each image is.
+def generate_image_dataset(samples_per_class=100):
+# This function should return two NumPy arrays:
+#   X: the flattened input data, which has shape (num_samples, 64)
+#   y: the labels for the input data, which has shape (num_samples,)
+    X = []
+    y = []
+
+    #blank img (label 0)
+    for _ in range(samples_per_class):
+        img = generate_blank()
+        X.append(img.flatten())
+        y.append(0)
+
+    #horizontal_line img (label 1)
+    for _ in range(samples_per_class):
+        img = generate_horizontal_line()
+        X.append(img.flatten())
+        y.append(1)
+
+    X = np.array(X)
+    y = np.array(y)
+
+    return X, y
+
+# print("Here's a blank image:\n", generate_blank())
+
+# print("Here's a horizontal line image:\n", generate_horizontal_line())
+
+X, y = generate_image_dataset(samples_per_class=2)
+print("Here's the X dataset shape when num_samples = 4:\n", X.shape)
+print("Here's the y dataset shape num_samples = 4:\n", y.shape)
+print("Sample img 1:\n", X[0].reshape(8, 8), "\nSample img 2:\n", X[1].reshape(8, 8))
+print("Sample label 1:", y[0], "\nSample label 2:", y[1])
+print("Sample img 3:\n", X[2].reshape(8, 8), "\nSample img 4:\n", X[3].reshape(8, 8))
+print("Sample label 3:", y[2], "\nSample label 4:", y[3])
